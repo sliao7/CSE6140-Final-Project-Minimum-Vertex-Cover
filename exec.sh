caa16cc99a7335c2dd5d9afda951c6a5c535118f
Raymond <mr.raymondsi@gmail.com>
2020-11-30 02:54:38 -0500

    Update exec.sh

--- exec.sh
+++ exec.sh
@@ -1,7 +1,6 @@
 for fn in DATA/*.graph; do
-	for seed in {1..2}; do
+	for seed in {1..10}; do
 		echo $fn $seed
-	 	python Python/main.py -inst DATA/jazz.graph -alg BnB -time 600 -seed $seed >> exec.log
+	 	python Python/main.py -inst $fn -alg BnB -time 600 -seed $seed >> exec_BnB.log
 	done  
-	break
 done
